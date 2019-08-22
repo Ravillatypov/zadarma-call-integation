7263f75f96b39af221a4facfa5eb600491d31d19
Ravil Latypov <r.latypov@napoleonit.ru>
2019-08-22 09:14:05 +0300

    for add zadarma

--- backend/api/notify.py
+++ backend/api/notify.py
@@ -4,7 +4,11 @@ from sanic.response import HTTPResponse
 from api import api_bp as bp
 
 
-@bp.route('/notify', methods=['POST'])
+@bp.route('/notify', methods=['POST', 'GET'])
 async def out_end(request):
     logger.info(request.form)
+    logger.info(request.args)
+    zd_echo = request.args.get('zd_echo')
+    if zd_echo:
+        return HTTPResponse(zd_echo)
     return HTTPResponse('ok')
