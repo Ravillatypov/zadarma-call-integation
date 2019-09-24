ed5a2ff83c631b38cef15ccad837b785427ccfcd
Ravil Latypov <r.latypov@napoleonit.ru>
2019-09-24 16:17:37 +0300

    add connector pool

--- backend/zadarma.py
+++ backend/zadarma.py
@@ -2,14 +2,14 @@ import base64
 import hmac
 import json
 import os
+from asyncio import sleep
 from collections import OrderedDict, defaultdict
 from hashlib import sha1, md5
 from urllib.parse import urlencode
 
-from sanic.log import logger
 import aiofiles
 import aiohttp
-from asyncio import sleep
+from sanic.log import logger
 
 
 class ZadarmaAPI(object):
@@ -31,10 +31,11 @@ class ZadarmaAPI(object):
         if is_sandbox:
             self.__url_api = 'https://api-sandbox.zadarma.com'
         self.pbx_id = None
+        self.conn = aiohttp.TCPConnector(ttl_dns_cache=3600, limit=10)
 
     async def call(self,
                    method: str,
-                   params: dict = {},
+                   params: dict = { },
                    request_type: str = 'GET',
                    format: str = 'json',
                    is_auth: bool = True) -> dict:
@@ -48,8 +49,7 @@ class ZadarmaAPI(object):
         :return: response
         """
         request_type = request_type.upper()
-        allowed_type = ['GET', 'POST', 'PUT', 'DELETE']
-        if request_type not in allowed_type:
+        if request_type not in ('GET', 'POST', 'PUT', 'DELETE'):
             request_type = 'GET'
         params['format'] = format
         auth_str = None
@@ -63,18 +63,18 @@ class ZadarmaAPI(object):
             sorted_dict_params = OrderedDict(sorted(params.items()))
             params_string = urlencode(sorted_dict_params)
             request_url += '?' + params_string
-            async with aiohttp.ClientSession() as session:
-                async with session.get(request_url, headers={'Authorization': auth_str}) as response:
+            async with aiohttp.ClientSession(connector=self.conn, headers={ 'Authorization': auth_str }) as session:
+                async with session.get(request_url) as response:
                     result = await response.json()
         elif request_type == 'POST':
-            async with aiohttp.ClientSession() as session:
-                async with session.post(request_url, headers={'Authorization': auth_str}, data=data) as response:
+            async with aiohttp.ClientSession(connector=self.conn, headers={ 'Authorization': auth_str }) as session:
+                async with session.post(request_url, data=data) as response:
                     result = await response.json()
         elif request_type == 'PUT':
-            async with aiohttp.ClientSession() as session:
-                async with session.post(request_url, headers={'Authorization': auth_str}, data=data) as response:
+            async with aiohttp.ClientSession(connector=self.conn, headers={ 'Authorization': auth_str }) as session:
+                async with session.put(request_url, data=data) as response:
                     result = await response.json()
-        logger.info({'result': result, 'method': method, 'type': request_type})
+        logger.info({ 'result': result, 'method': method, 'type': request_type, 'data': data })
         return result
 
     def __get_auth_string_for_header(self, method: str, params: dict) -> str:
@@ -96,7 +96,6 @@ class ZadarmaAPI(object):
         return await self.call('/v1/request/callback/', {'from': a_number, 'to': b_number})
 
     async def set_redirect(self, sip: str, to_number: str) -> dict:
-        logger.info({'pbx_number': sip, 'destination': to_number})
         return await self.call('/v1/pbx/redirection/', {
             'pbx_number': f'{self.pbx_id}-{sip}',
             'status': 'on',
@@ -114,7 +113,7 @@ class ZadarmaAPI(object):
         filename = os.path.join(dir_path, os.path.basename(link))
 
         async with aiofiles.open(filename, 'wb') as fd:
-            async with aiohttp.ClientSession() as session:
+            async with aiohttp.ClientSession(connector=self.conn) as session:
                 async with session.get(link) as response:
                     while True:
                         chunk = await response.content.read(1024)
@@ -144,15 +143,18 @@ class ZadarmaAPI(object):
                     self._dict[k] -= 1
                     result = k
             await sleep(5)
+        logger.info({ 'sip_number': result, 'numbers_dict': self._dict })
         return result
 
     async def get_lock(self, number: str):
         while self._lockers[number]:
             await sleep(1)
         self._lockers[number] = True
+        logger.info(f'locked sip number {number}')
 
     def release_lock(self, number: str):
         self._lockers[number] = False
+        logger.info(f'released sip number {number}')
 
     def release_number(self, number: str):
         if self._dict[number] < self.max_channels:
